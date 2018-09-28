87682391307d7e3370ae84f20686fe34ef0155db
Luxelh <43620483+Luxelh@users.noreply.github.com>
2018-09-28 08:48:00 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -43,3 +43,18 @@ client.on('message', message => {
         message.reply("salut");
         console.log('Le bot dit bonjour');
     }
+
+    if(message.content === prefix + "help"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor("#0099FF")
+        .setTitle("Commandes de la FLEM")
+        .addField("!help","Affiche les commandes disponible sur le BOT FLEM.")
+        .addField("Bonjour", "Le bot te repondra!")
+        .addField("!statistiques", "Pour connaître tes statistiques d'utilisateur Discord.")
+        .addField("!bot", "Le bot répetera à interval régulier le message qui suivra la commande. Seul le staff est autorisé à l'utiliser.")
+        .addField("!stop", "Cette commande arrêtera le messsage.")
+        .setFooter("Pour plus d'information se renseigner auprès du technicien Discord (@shoumi#7812)")
+        .addField("!clear XX", "Permet de supprimer le nombre de message indiqué, seul le staff est autorisé à utiliser cette fonction!")
+        message.channel.sendMessage(help_embed)
+        console.log("Menu !help ouvert!")
+    }
